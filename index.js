13f1167369f196572dfcdbc126165dbda5e06adf
Silent Ghost <abirloading@gmail.com>
2024-02-27 15:26:04 +0600

    admin route setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 9000;
 
@@ -24,11 +25,53 @@ const ItemCollection = client.db("SnakeEye").collection("ProductCollection");
 const blogsCollection = client.db("SnakeEye").collection("blogsCollection");
 const userCollection = client.db("SnakeEye").collection("userCollection");
 const commentCollection = client.db("SnakeEye").collection("commentCollection");
+const cartCollection = client.db("SnakeEye").collection("cartCollection");
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.Access_Token, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter); //* Find where we put the user
+      if (user?.role !== "admin") {
+        res.status(403).send({ error: true, message: "forbidden message" });
+        next();
+      }
+    };
+
+    // jwt
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.Access_Token, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // product route
 
     app.get("/product", async (req, res) => {
       const result = await ItemCollection.find().toArray();
@@ -42,6 +85,21 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/product", async (req, res) => {
+      const item = req.body;
+      const result = await ItemCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await ItemCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // blog route
+
     app.get("/blogs", async (req, res) => {
       const result = await blogsCollection.find().toArray();
       res.send(result);
@@ -51,6 +109,13 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await blogsCollection.findOne(filter);
+      res.send(result);
+    });
+
+    // user route
+
+    app.get("/user", verifyJWT, verifyAdmin,async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result);
     });
 
@@ -64,6 +129,25 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/user/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/user/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // comment route
 
     app.get("/comment", async (req, res) => {
@@ -74,6 +158,55 @@ async function run() {
     app.post("/comment", async (req, res) => {
       const comment = req.body;
       const result = await commentCollection.insertOne(comment);
+      res.send(result);
+    });
+
+    // cart route
+
+    app.get("/carts", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+
+      //email validation
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "access forbidden" });
+      }
+
+      const filter = { email: email };
+      const result = await cartCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // * Admin Check
+
+    app.get("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cart = req.body;
+      const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(filter);
       res.send(result);
     });
 
